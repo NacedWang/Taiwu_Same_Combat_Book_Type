940bf6543956bfbd41df60b2467208e59eac03cc
Naced <869521569@qq.com>
2022-10-08 21:38:09 +0800

    总纲可配置 ，目前书全部完整，且正练

--- SameCombatBookType/Config.lua
+++ SameCombatBookType/Config.lua
@@ -1,12 +1,34 @@
 return {
-	Title = "太吾出版社【功法书章节转化器】",
-	Version = 1,
-	Source = 0,
-	BackendPlugins = {"SameCombatBookType.dll"}, 
+	Description = "秦始皇统一六国，都能车同轨书同文，你说你个太吾世界，咋就这么多非法出版社呢？\
+本MOD可以自定义获取功法书的总纲、正逆练章节。\
+源码：https://github.com/NacedWang/Taiwu_Same_Combat_Book_Type",
+	Version = 2,
+	Source = 1,
 	Author = "Naced",
-	Description = "可以自定义获取功法书的总纲、正逆练章节\
-源码：https://github.com/NacedWang/Taiwu_LongDaoSSR_new",
+	DefaultSettings = 
+	{
+		[1] = 
+		{
+			DisplayName = "总纲类型",
+			Description = "总纲类型",
+			Key = "BookIndexType",
+			Options = 
+			{
+				[1] = "承（刚正）",
+				[2] = "合（任善）",
+				[3] = "解（中庸）",
+				[4] = "异（叛逆）",
+				[5] = "独（唯我）"
+			},
+			SettingType = "Dropdown",
+			DefaultValue = 2
+		}
+	},
+	BackendPlugins = 
+	{
+		[1] = "SameCombatBookType.dll"
+	},
+	FileId = 2871831350,
 	Cover = "logo.png",
-	DefaultSettings = {
-	}
+	Title = "太吾出版社【功法书章节转化器】",
 }
--- SameCombatBookType/SameCombatBookType.cs
+++ SameCombatBookType/SameCombatBookType.cs
@@ -1,5 +1,9 @@
-﻿using GameData.Domains;
+﻿using GameData.ArchiveData;
+using GameData.Common;
+using GameData.Domains;
+using GameData.Domains.Character;
 using GameData.Domains.Item;
+using GameData.Domains.Merchant;
 using GameData.Utilities;
 using HarmonyLib;
 using System;
@@ -22,7 +26,7 @@ namespace SameCombatBookType
         }
 
         // 总纲
-        private static int IndexType;
+        private static int BookIndexType;
 
         public override void Initialize()
         {
@@ -33,19 +37,66 @@ namespace SameCombatBookType
         public override void OnModSettingUpdate()
         {
             AdaptableLog.Info("SameCombatBookType OnModSettingUpdate");
-            DomainManager.Mod.GetSetting(base.ModIdStr, "SameMorality", ref SameCombatBookType.IndexType);
-            AdaptableLog.Info(string.Format("SameCombatBookType setting : \n IndexType :{0} \n ", IndexType));
+            DomainManager.Mod.GetSetting(base.ModIdStr, "BookIndexType", ref SameCombatBookType.BookIndexType);
+            AdaptableLog.Info(string.Format("SameCombatBookType setting : \n BookIndexType :{0} \n ", BookIndexType));
         }
 
-        [HarmonyPrefix, HarmonyPatch(typeof(ItemDomain), "AddElement_SkillBooks", new Type[] { typeof(int) ,typeof(SkillBook) }, new ArgumentType[] { ArgumentType.Normal , ArgumentType.Normal })]
-        public static bool prefixAddElementSkillBooks(ref ItemDomain __instance, int objectId, SkillBook instance)
+        [HarmonyPrefix, HarmonyPatch(typeof(MerchantDomain), "ExchangeBook")]
+        public unsafe static bool prefixExchangeBook(ref MerchantDomain __instance, DataContext context, int npcId, Inventory getBook, int selfAuthority, int npcAuthority)
         {
-            if (instance.IsCombatSkillBook())
+            AdaptableLog.Info("ExchangeBook");
+            if (getBook == null)
             {
-                AdaptableLog.Info("patch pre AddElement_SkillBooks : " + Convert.ToString(objectId));
-                AdaptableLog.Info("patch pre AddElement_SkillBooks :" + Convert.ToString(instance.GetCombatSkillType()));
+                return true;
+            }
+            else
+            {
+                AdaptableLog.Info("prefixExchangeBook : " + Convert.ToString(npcId));
+                AdaptableLog.Info("prefixExchangeBook : " + Convert.ToString(selfAuthority));
+                AdaptableLog.Info("prefixExchangeBook : " + Convert.ToString(npcAuthority));
+                Inventory newGetBook = new Inventory();
+                foreach ((ItemKey item, int i) in getBook.Items)
+                {
+                    if (item.ItemType == 10)
+                    {
+                        AdaptableLog.Info("   ---   " + Convert.ToString(item.ItemType));
+                        AdaptableLog.Info("   ---   " + Convert.ToString(item.TemplateId));
+                        AdaptableLog.Info("   ---   " + Convert.ToString(item.Id));
+                        SkillBook element_SkillBooks = DomainManager.Item.GetElement_SkillBooks(item.Id);
+                        AdaptableLog.Info("   ---   " + Convert.ToString(element_SkillBooks.GetName()));
+                        if (element_SkillBooks.IsCombatSkillBook())
+                        {
+                            // 残缺状态
+                            element_SkillBooks.SetPageIncompleteState((ushort)0, context);
+                            // 耐久
+                            //element_SkillBooks.SetMaxDurability(25, context);
+                            //element_SkillBooks.SetCurrDurability(25, context);
+                            // 总纲
+                            Traverse.Create(element_SkillBooks).Field("_pageTypes").SetValue((byte)BookIndexType);
+                            //element_SkillBooks.SetModificationState(())
+                            // element_SkillBooks.InvalidateSelfAndInfluencedCache(5, context);
+                            //bool isArchive = element_SkillBooks.CollectionHelperData.IsArchive;
+                            /*byte b = 1;
+                            if (isArchive)
+                            {
+                                byte* ptr = OperationAdder.DynamicObjectCollection_SetFixedField<int>(element_SkillBooks.CollectionHelperData.DomainId, element_SkillBooks.CollectionHelperData.DataId, item.Id, 11U, 1);
+                                *ptr = b;
+                                ptr++;
+                            }*/
+                            Character element_Objects = DomainManager.Character.GetElement_Objects(DomainManager.Taiwu.GetTaiwuCharId());
+                            element_Objects.AddInventoryItem(context, item, 1);
+                        }
+                        AdaptableLog.Info("  ===================  ");
+                    }
+                    else
+                    {
+                        newGetBook.OfflineAdd(item, i);
+                    }
+                }
+                getBook = newGetBook;
             }
             return true;
         }
+
     }
 }
